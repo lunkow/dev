1671b9c58db95b3110bbc36955257edc7f770c15
Anton Lunkov <lunkow@live.ru>
2024-11-11 18:51:59 +0300

    Update nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: '/',
+  },
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  nitro: {
+    output: {
+      dir: 'docs',
+      publicDir: 'docs',
+    },
+  },
 })
